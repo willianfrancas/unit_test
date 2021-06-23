ccda638a86b1ae4889e71e73ca25aec0e2aacf22
willianfrancas <willianfrancas@gmail.com>
2021-06-22 21:47:41 -0300

    module_03: aria-live / aria-atomic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LikeWidgetModule } from './shared/components/like-widget/like-widget.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LikeWidgetModule
   ],
   providers: [],
   bootstrap: [AppComponent]
